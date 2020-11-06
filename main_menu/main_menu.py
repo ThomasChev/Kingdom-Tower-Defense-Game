12c408242794c319f15fb5863cda160fc25fd20a
ThomasChev <thomas.cheval@outlook.com>
2020-11-06 21:21:41 +0100

    add character images and text in the main menu

--- main_menu/main_menu.py
+++ main_menu/main_menu.py
@@ -16,19 +16,29 @@ medium_btn = pygame.image.load(os.path.join("game_assets/menu/", "medium_btn.png
 hard_btn = pygame.image.load(os.path.join("game_assets/menu/", "hard_btn.png"))
 
 # Attack Towers
-info_shin_img = pygame.image.load(os.path.join("game_assets/menu/", "info_shin.png"))
-info_moubu_img = pygame.image.load(os.path.join("game_assets/menu/", "info_moubu.png"))
-info_kanki_img = pygame.image.load(os.path.join("game_assets/menu/", "info_kanki.png"))
-info_ouhon_img = pygame.image.load(os.path.join("game_assets/menu/", "info_ouhon.png"))
+img_shin = pygame.image.load(os.path.join("game_assets/quin_towers/shin_info/", "shin_img.png"))
+img_moubu = pygame.image.load(os.path.join("game_assets/quin_towers/moubu_info/", "moubu_img.png"))
+img_kanki = pygame.image.load(os.path.join("game_assets/quin_towers/kanki_info/", "kanki_img.png"))
+img_ouhon = pygame.image.load(os.path.join("game_assets/quin_towers/ouhon_info/", "ouhon_img.png"))
+info_shin = pygame.image.load(os.path.join("game_assets/quin_towers/shin_info/", "shin_info.png"))
+info_moubu = pygame.image.load(os.path.join("game_assets/quin_towers/moubu_info/", "moubu_info.png"))
+info_kanki = pygame.image.load(os.path.join("game_assets/quin_towers/kanki_info/", "kanki_info.png"))
+info_ouhon = pygame.image.load(os.path.join("game_assets/quin_towers/ouhon_info/", "ouhon_info.png"))
 
 # Support Towers + Fortress
-info_fortress_img = pygame.image.load(os.path.join("game_assets/menu/", "info_fortress.png"))
-info_kyoukai_img = pygame.image.load(os.path.join("game_assets/menu/", "info_kyoukai.png"))
-info_ten_img = pygame.image.load(os.path.join("game_assets/menu/", "info_ten.png"))
-info_ryo_img = pygame.image.load(os.path.join("game_assets/menu/", "info_ryo.png"))
+img_fortress = pygame.image.load(os.path.join("game_assets/fortress/fortress_info/", "fortress_img.png"))
+img_kyoukai = pygame.image.load(os.path.join("game_assets/support_towers/kyoukai_info/", "kyoukai_img.png"))
+img_ten = pygame.image.load(os.path.join("game_assets/support_towers/ten_info/", "ten_img.png"))
+img_ryo = pygame.image.load(os.path.join("game_assets/support_towers/ryo_info/", "ryo_img.png"))
+info_fortress = pygame.image.load(os.path.join("game_assets/fortress/fortress_info/", "fortress_info.png"))
+info_kyoukai = pygame.image.load(os.path.join("game_assets/support_towers/kyoukai_info/", "kyoukai_info.png"))
+info_ten = pygame.image.load(os.path.join("game_assets/support_towers/ten_info/", "ten_info.png"))
+info_ryo = pygame.image.load(os.path.join("game_assets/support_towers/ryo_info/", "ryo_info.png"))
 
 pygame.mixer.pre_init()
 pygame.mixer.init()
+
+
 
 pad_x = 75
 pad_y = 5
@@ -41,25 +51,59 @@ class MainMenu:
         self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.win = win
         self.font = pygame.font.Font("game_assets/fonts/SF Atarian System.ttf", 48)
-        
+        self.font_20 = pygame.font.Font("game_assets/fonts/SF Atarian System.ttf", 18)
+
+        # play
         self.btn = (self.width/2 - start_btn.get_width()/2, 225, start_btn.get_width(), start_btn.get_height())
+        
+        # get lore info
         self.lore = (self.width/2 - lore_btn.get_width()/2, 300, lore_btn.get_width(), lore_btn.get_height())
+        
+        # get character infos
         self.info = (self.width/2 - info_btn.get_width()/2, 375, info_btn.get_width(), info_btn.get_height())
+        self.x = self.info[0] - pad_x - info_shin.get_width() + info_btn.get_width()
+        self.y = self.info[1]
+        self.add_y = info_shin.get_height() + pad_y
+        self.shin = (self.x, self.y + 0*self.add_y, info_shin.get_width(), info_shin.get_height())
+        self.moubu = (self.x, self.y + 1*self.add_y, info_moubu.get_width(), info_moubu.get_height())
+        self.kanki = (self.x, self.y + 2*self.add_y, info_kanki.get_width(), info_kanki.get_height())
+        self.ouhon = (self.x, self.y + 3*self.add_y, info_ouhon.get_width(), info_ouhon.get_height())
+        self.fortress = (self.info[0] + pad_x, self.y + 0*self.add_y, info_fortress.get_width(), info_fortress.get_height())
+        self.kyoukai = (self.info[0] + pad_x, self.y + 1*self.add_y, info_kyoukai.get_width(), info_kyoukai.get_height())
+        self.ten = (self.info[0] + pad_x, self.y + 2*self.add_y, info_ten.get_width(), info_ten.get_height())
+        self.ryo = (self.info[0] + pad_x, self.y + 3*self.add_y, info_ryo.get_width(), info_ryo.get_height())
+
+        self.img_names = ['shin', 'moubu', 'kanki', 'ouhon', 'fortress', 'kyoukai', 'ten', 'ryo']
+        self.character_infos = [self.shin, self.moubu, self.kanki, self.ouhon, self.fortress, self.kyoukai, self.ten, self.ryo]
+        self.info_imgs = [info_shin, info_moubu, info_kanki, info_ouhon, info_fortress, info_kyoukai, info_ten, info_ryo]
+        self.character_imgs = [(img_shin, (900, 225), text_shin, (700,225)), (img_moubu, (870, 225), text_moubu, (670,225)), (img_kanki, (900, 225), text_kanki, (700,225)), (img_ouhon, (845, 225), text_ouhon, (645,225)), (img_fortress, (5, 325), text_fortress, (-80,325)), (img_kyoukai, (48, 225), text_kyoukai, (48, 225)), (img_ten, (57, 225), text_ten, (57, 225)), (img_ryo, (31, 225), text_ryo, (31, 225))]
+        
+        # change level
         self.level = (self.width/2 - level_btn.get_width()/2, 450, level_btn.get_width(), level_btn.get_height())
         self.easy = (self.width/2 - easy_btn.get_width()/2, 525, easy_btn.get_width(), easy_btn.get_height())
         self.medium = (self.width/2 - medium_btn.get_width()/2, 560, medium_btn.get_width(), medium_btn.get_height())
         self.hard = (self.width/2 - hard_btn.get_width()/2, 595, hard_btn.get_width(), hard_btn.get_height())
-        
         self.game_level = "Easy"
         self.lvls = ["Easy", "Medium", "Hard"]
         self.levels = [self.easy, self.medium, self.hard]
-        self.buttons = [self.lore, self.info, self.level]
+        
         self.btn_names = ["lore", "info", "level"]
-
+        self.buttons = [self.lore, self.info, self.level]
         self.show_lore = False
         self.show_info = False
         self.show_level = False
-        self.shows = {"lore" : self.show_lore, "info":self.show_info, "level":self.show_level}
+        self.show_shin = False
+        self.show_moubu = False
+        self.show_kanki = False
+        self.show_ouhon = False
+        self.show_fortress = False
+        self.show_kyoukai = False
+        self.show_ten = False
+        self.show_ryo = False
+        self.show_img = False
+        self.shows = {"lore" : self.show_lore, "info":self.show_info, "level":self.show_level, 'shin':self.show_shin, 'moubu':self.show_moubu, 'kanki':self.show_kanki, 'ouhon':self.show_ouhon, 'fortress':self.show_fortress, 'kyoukai':self.show_kyoukai, 'ten':self.show_ten, 'ryo':self.show_ryo, 'img':self.show_img}
+        self.right = False
+        self.left = False
 
     def run(self):
         pygame.mixer.music.load(os.path.join("game_assets/sounds/", "melody.wav"))
@@ -90,7 +134,7 @@ class MainMenu:
                         del game
 
                     # if you click on other buttons
-                    for i, (button, name) in enumerate(zip(self.buttons, self.btn_names)):
+                    for button, name in zip(self.buttons, self.btn_names):
                         if self.click(button, x, y):
                             play_sound(1,"toggle.wav", 600)
                             for key in self.shows:
@@ -98,6 +142,26 @@ class MainMenu:
                                     self.shows[key] = False
                             self.shows[name] = not(self.shows[name])
                             break
+
+                    # if you click on Character imgs
+                    if self.shows["info"]:
+                        for button, name in zip(self.character_infos, self.img_names):
+                            if self.click(button, x, y):
+                                play_sound(1,"beep_menu.wav",300)
+                                for key in self.shows:
+                                    if key != name:
+                                        self.shows[key] = False
+                                self.shows["info"] = True
+                                self.shows["img"] = True
+                                self.shows[name] = not(self.shows[name])
+
+                                if x < self.width/2:
+                                    self.left = True
+                                    self.right = False
+                                elif x >= self.width/2:
+                                    self.right = True
+                                    self.left = False
+                                break
 
                     # if you click on Easy/Medium/Hard buttons
                     if self.shows["level"]:
@@ -111,13 +175,16 @@ class MainMenu:
         pygame.quit()
 
     def draw(self):
+
+        # draw background and text
         self.win.blit(self.bg, (0,0))
         text = self.font.render(self.game_level, 2, rgb(255,255,255))
         self.win.blit(text, (self.width/2 - text.get_width()/2, self.height - 10 - text.get_height()))
+        
+        # draw Start, Lore, Info, Level buttons
         self.win.blit(start_btn, (self.btn[0], self.btn[1]))
         self.win.blit(lore_btn, (self.lore[0], self.lore[1]))
         self.win.blit(info_btn, (self.info[0], self.info[1]))
-        
         if self.game_level == "Easy":
             level_btn = pygame.image.load(os.path.join("game_assets/menu/", "level1_btn.png"))
         elif self.game_level == "Medium":
@@ -126,39 +193,53 @@ class MainMenu:
             level_btn = pygame.image.load(os.path.join("game_assets/menu/", "level3_btn.png"))
         self.win.blit(level_btn, (self.level[0], self.level[1]))
 
-        # print("at draw:", self.shows)
+        # draw lore imgs
         if self.shows["lore"]:
-            print("lore")
-            # right image
+            # right img
             x = self.lore[0] + pad_x
             y = self.lore[1]
             self.win.blit(lore_img, (x, y))
 
-            # left image
+            # left img
             x = self.lore[0] - pad_x - lore2_img.get_width() + lore_btn.get_width()
             y = self.lore[1]
             self.win.blit(lore2_img, (x, y))
 
-        if self.shows["info"]:
-            
-            # Support Towers + Fortress, right images
-            x = self.info[0] + pad_x
-            y = self.info[1]
-            add_y = info_shin_img.get_height() + pad_y
-            self.win.blit(info_fortress_img, (x, y + 0*add_y ))
-            self.win.blit(info_kyoukai_img, (x, y + 1*add_y ))
-            self.win.blit(info_ten_img, (x, y + 2*add_y ))
-            self.win.blit(info_ryo_img, (x, y + 3*add_y ))
+        # draw character infos, attack Towers (left), support Towers (right)
+        if self.shows["info"] and not self.shows['img']:
+            for button, img in zip(self.character_infos, self.info_imgs):
+                self.win.blit(img, (button[0], button[1]))
 
-            # Attack Towers, left images
-            x = self.info[0] - pad_x - info_shin_img.get_width() + info_btn.get_width()
-            y = self.info[1]
-            add_y = info_shin_img.get_height() + pad_y
-            self.win.blit(info_shin_img, (x, y + 0*add_y ))
-            self.win.blit(info_moubu_img, (x, y + 1*add_y ))
-            self.win.blit(info_kanki_img, (x, y + 2*add_y ))
-            self.win.blit(info_ouhon_img, (x, y + 3*add_y ))
+        # draw characters
+        corr = 0
+        for name, img in zip(self.img_names, self.character_imgs):
+            if self.shows[name]:
+                x = img[1][0]
+                y = img[1][1]
+                self.win.blit(img[0], (x, y))
 
+                if self.right:           
+                    corr = img[0].get_width() + 10
+                
+                size = (900, 50)
+                text = img[2]
+                x = img[3][0] + corr
+                y = img[3][1]
+                self.blit_text(size, text, (x, y), self.font_20, rgb(255,255,255))
+
+                for i, (button, info) in enumerate(zip(self.character_infos, self.info_imgs)):
+                    if self.left:
+                        if i >= 4 :
+                            pass
+                        else:
+                            self.win.blit(info, (button[0], button[1]))
+                    if self.right:
+                        if i < 4:
+                            pass
+                        else:
+                            self.win.blit(info, (button[0], button[1]))
+
+        # draw level buttons
         if self.shows["level"]:
             x = self.easy[0]
             y = self.easy[1]
@@ -182,6 +263,23 @@ class MainMenu:
                 return True
         return False
 
+    def blit_text(self, surface, text, pos, font, color=pygame.Color('black')):
+        words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+        space = font.size(' ')[0]  # The width of a space.
+        max_width, max_height = surface[0], surface[1]
+        x, y = pos
+        for line in words:
+            for word in line:
+                word_surface = font.render(word, 0, color)
+                word_width, word_height = word_surface.get_size()
+                if x + word_width >= max_width:
+                    x = pos[0]  # Reset the x.
+                    y += word_height  # Start on new row.
+                self.win.blit(word_surface, (x, y))
+                x += word_width + space
+            x = pos[0]  # Reset the x.
+            y += word_height  # Start on new row.
+
 def play_sound(*args):
     if len(args) == 3:
         a,b,c = args[0],args[1],args[2]
@@ -189,3 +287,78 @@ def play_sound(*args):
     elif len(args) == 2:
         a,b = args[0],args[1]
         pygame.mixer.Channel(a).play(pygame.mixer.Sound(os.path.join("game_assets/sounds/", b)))
+
+
+#### TEXT ####
+
+text_shin = "Shin is the main protagonist of Kingdom. \nHe is a General of Qin and the leader of the Hi Shin Army.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down a large number of soldiers.\n" \
+        "\n" \
+        " - Damage: Low\n" \
+        " - Range: Enhanced Range\n" \
+        " - Cost : Cheap\n" \
+        " - Ability: None"
+
+text_moubu = "Mou Bu is a Great General of Qin, the leader of Mou Bu Army, " \
+        "and the head of the Mou Family, also a former member of the 'Four Pillars' of Ryo Fui.\n" \
+        "Mou Bu has been called the 'Unrivaled Strongest Man in Qin' because of his brute force approach to battle.\n" \
+        "\n" \
+        "Mace User: he is able to easily cut down armor soldiers and Chu's Units.\n" \
+        "\n" \
+        " - Damage: Enhanced Damage\n" \
+        " - Range: Low\n" \
+        " - Cost: Expensive\n" \
+        " - Ability: Special Damage to Chu's Units (Warriors, Elephants, Boats)"
+
+text_kanki = "Mou Bu is a Great General of Qin, the leader of Mou Bu Army, " \
+        "and the head of the Mou Family, also a former member of the 'Four Pillars' of Ryo Fui..\n" \
+        "Mou Bu has been called the 'Unrivaled Strongest Man in Qin' because of his brute force approach to battle.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down several units and Wei's Siege Units.\n" \
+        "\n" \
+        " - Damage: Enhanced Attack Speed\n" \
+        " - Range: Low\n" \
+        " - Cost: Medium\n" \
+        " - Ability: Special Damage to Wei's Siege Units (Catapult, Balista)"
+
+text_ouhon = "Shin is the main protagonist of Kingdom. \nHe is a General of Qin and the leader of the Hi Shin Army.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down a large number of soldiers.\n" \
+        "\n" \
+        " - Damage: Low\n" \
+        " - Range: Enhanced Range\n" \
+        " - Cost : Cheap\n" \
+        " - Ability: None"
+
+text_fortress = "Qin City, must be besieged \nby enemies along their path.\n" \
+        "\n" \
+        " - Health: 500/1000/1500\n" \
+        " - Weakness: Wei Siege Units\n" \
+
+text_kyoukai = "Shin is the main protagonist of Kingdom. \nHe is a General of Qin and the leader of the Hi Shin Army.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down a large number of soldiers.\n" \
+        "\n" \
+        " - Damage: Low\n" \
+        " - Range: Enhanced Range\n" \
+        " - Cost : Cheap\n" \
+        " - Ability: None"
+
+text_ten = "Shin is the main protagonist of Kingdom. \nHe is a General of Qin and the leader of the Hi Shin Army.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down a large number of soldiers.\n" \
+        "\n" \
+        " - Damage: Low\n" \
+        " - Range: Enhanced Range\n" \
+        " - Cost : Cheap\n" \
+        " - Ability: None"
+
+text_ryo = "Shin is the main protagonist of Kingdom. \nHe is a General of Qin and the leader of the Hi Shin Army.\n" \
+        "\n" \
+        "Swordman: he is able to easily cut down a large number of soldiers.\n" \
+        "\n" \
+        " - Damage: Low\n" \
+        " - Range: Enhanced Range\n" \
+        " - Cost : Cheap\n" \
+        " - Ability: None"
